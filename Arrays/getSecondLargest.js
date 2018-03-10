7e99116594b5fb950dfea1ce31cfd696a5f38103
Ketki Rajendra Mahajan <31837601+KetkiMahajan@users.noreply.github.com>
2018-03-09 20:02:20 -0600

    getSecondLargest

--- Arrays/getSecondLargest.js
+++ Arrays/getSecondLargest.js
@@ -1,8 +1,8 @@
 function getSecondLargest(nums) {
     // Complete the function
-   const unique = Array.from(new Set(nums));
-const reverseSorted = unique.sort(function(a, b){return b - a})
-const second = reverseSorted[1];
-return (second);
+    const unique = Array.from(new Set(nums));
+    const reverseSorted = unique.sort(function(a, b){return b - a})
+    const second = reverseSorted[1];
+    return (second);
     
 }
